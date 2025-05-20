a521d6e4709c3d4002be7366138f1580aedb1ae9
MuhammadChoirulRizal <mchoirulrizal9@gmail.com>
2025-05-20 10:57:48 +0700

    operasi push

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -76,4 +76,11 @@ int main()
         cout << "4.exit\n";
         cout << "enter your choice :";
         cin >> choice;
-        
+        switch (choice)
+        {
+        case 1:
+            cout << "enter the value to push :";
+            cin >> value;
+            stact.push(value);
+            break;
+       
